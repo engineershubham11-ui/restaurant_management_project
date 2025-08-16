d0211a83c8729e90bc03bb1aa92ca42c5cf4449f
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-16 13:48:47 +0530

    33018

--- account/views.py
+++ account/views.py
@@ -1,20 +1,25 @@
-from django.http import jsonresponse 
-from django.db import detabaseError
-from .models import MenuItem
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <title> Restaurant Homepage</title>
+   <style>
+     .logo{
+        display:block;
+        margin:30px auto;
+        width:200px;
+        height: auto;
 
-def menu_items_view(request):
-    try:
-        items = MenuItem.object.all()
-        deta = [{"id": item.id, "name": item.name, "price": item.price } for item in items]
-        return jsonresponse({"menu_items": data }, status=200)
+     }
+    </style>
+</head>
+<body>
+    <header>
+    <img src="{% static 'images/ logo.png' %}" alt="Restaurant Logo" class="logo">
+    </header>
 
-        except DatabaseError as e:
-            return jsonresponse(
-                {"error":"we are experiencing database issues. please try again later."},
-                status=500
-            )
-except Exception as e:
-    return jsonresponse(
-        {"error":"something wenr wrong. please contact support."},
-        status=500
-    )
+    <main>
+    <p> Enjoy the finest dining experience!</p>
+    </main>
+    </body>
+    </html>
