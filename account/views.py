178781f637137316862ba59d9212bf39e2b538f7
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-17 11:59:37 +0530

    33991

--- account/views.py
+++ account/views.py
@@ -2,88 +2,36 @@
 <html lang="en">
 <head>
    <meta charset="UTF-8">
-   <title> Restaurant Homepage</title>
-   <style>
-    body {
-       font-family:arial, helvetica, sans-serif;
-       backgroung-color: #fafafa;
-       color: #333;
-       margin: 0;
-       padding: 0;
-     }
-     header{
-        backgroung-color: #8B0000,
-        padding: 20px;
-        text-align: center;
-     }
-     .logo{
-        width:180px;
-        height:auto;
-     }
-     h1{
-        color: #8B0000;
-        text-align:center
-        margin-top: 30px;
-     }
-     p{
-        font-size:18px;
-        text-align: center;
-        margin: 15px auto;
-        max-width: 600px;
-        line-height: 1.6;
-     }
-     main {
-        padding:20px;
-     }
-     footer {
-        backgroung-color:#333;
-        color:white;
-        text-align:center;
-        padding: 10px;
-        margin-top:40px;
-     }
+   <title> contact us </title>
+   <script>
+    
+    function validateForm(){
+        let name = document.forms["contactForm"]["name"].value;
+        let email = document.Forms["contactForm"]["email"].value;
 
-    </style>
+        if (name ==="" || email === ""){
+            alert("please fill in both name and Email fields.");
+            return false; 
+        }
+        return true;
+    }
+
+    </script>
 </head>
 <body>
-    <header>
-    <img src="{% static 'images/ logo.png' %}" alt="Restaurant Logo" class="logo">
-    </header>
+     <h2> contact us </h2>
+     <from name="contactForm" onsubmit="return validateFrom()">
+     <label for="name">Name:</label><br>
+     <imput type="text" id="name" name="name"><br><br>
 
-    <main>
-    <p> Enjoy the finest dining experience!</p>
-    </main>
-         <footer>
-         <p> 2025 our restaurant . all right reserved.</p>
-        </footer>
+     label for="email">Email:</label><br>
+     <input type="email" id="email" name="email"><br><br>
+
+     <label for="message">Message:</label><br>
+     <textarea id="message" name="message"></textarea><br><br>
+
+     <button type="submit">submit</button>
+   
+        </form>
  </body>
 </html>
-
-
-
-
-
-
-{% extend "base.html" % }
-
-
-
-{% block title % } Restaurant -my restaurant {% endblock %}
-
-{% block header %}
-   <h1 style="color: white; background-color: #8B0000; padding: 15px; margin: 0;">
-   
-reservation
-
-   </h1>
-
-   {% endblock %}
-
-   {% block context %}
-
-   <div style="text-align: center; margin-top: 50px;">
-   <h2>reservation coming soon!</h2>
-      <p>we're working on our online reservation system. plese call us at <strong>(123)456-7890</strong> to book a table for now</p>
-      </div>
-    {% endblock %}
-    
