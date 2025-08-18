e5a529fdffca46ef1dc35d86df0e730312038645
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-18 21:33:37 +0530

    35452

--- account/views.py
+++ account/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponse
-from .models import restaurant 
+from django.db import models 
 
-def mennu_page(request):
-    items = menuItem.objects.all()
-    return render(request, 'menu.html',{'items': items})
+class menuItem(models.model):
+    name = models.charField(max_length=200)
+    description = models.TextField(blank=true, null=true)
+    price = models.decimalField(max_digit=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - ${self.price}"
     
     
