89b22c701743ae6f50420047fb52bd7d45b065da
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-18 21:26:17 +0530

    35453

--- account/views.py
+++ account/views.py
@@ -1,23 +1,8 @@
 from django.http import HttpResponse
 from .models import restaurant 
 
-def home(request):
-    restaurant = restaurant.object.first()
-    name = restaurant.name if restaurant else "our Restaurant "
-    return HttpResponse(f"<h1>welcome to {name}</h1>")
-
-
-
-
-#setting.py
-
-RESTAURANT_NAME = "Food Paradise"
-
-#urls.py
-
-from django.urls import path
-from .view import home
-
-urlpattern = [
-    path('',home, name='home'),
-]
+def mennu_page(request):
+    items = menuItem.objects.all()
+    return render(request, 'menu.html',{'items': items})
+    
+    
