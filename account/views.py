e28961624ea69f0b9e93dee629536b7f3f368a4a
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-19 21:27:39 +0530

    35447

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 #models.py
 from django.db import models 
+from django.contrib.auth.models import user
 
 class menu(models.model):
     name = models.CharField(max_length=100)
@@ -12,13 +13,22 @@ class menu(models.model):
 
 
 class order(models.model):
-    customer_name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=true)
-    items = models.ManyToManyField(menu)
+    STATUS_CHOICES =[
+        ('PENDING', 'Pending'),
+        ('PROCESSING', 'Processing'),
+        ('COMPLETED', 'Completed')
+        ('CANCELLED', 'cancelled')
+    ]
+
+    customer = Models.ForeignKey(user, on_delete=models.CASCADE, RELATED_name="orders")
+    item = models.ManyToManyField(menu, related_name="orders")
+    total_amount = model.DecimalField(max_length=8, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES,default='PENDING')
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
-        return f"order #{self.id} by {self.customer_name}"
+        return f"order #{self.id} by {self.customer.username} -{self.status}"
 
 
 #admin.py
@@ -33,7 +43,7 @@ class MenuAdmin(admin.ModeIAdmin):
 
 @admin.register(order)
 class orderAdmin(admin.modelAdmin):
-    list_display = ("id", "customer_name","created_at")
+    list_display = ("id", "customer","total_amount", "status", "created_at")
     search_fields = ("customer_name",)
     filter_horizontal = ("items",)
 
