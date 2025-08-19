c36ebac0f2b37225b5eecd96f9744942deaf183a
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-19 12:34:22 +0530

    35450

--- account/views.py
+++ account/views.py
@@ -1,37 +1,29 @@
-from django.shortcuts import render
-from .model import MenuItems
+from django.db import models
 
-def home(request):
-    context = {
-        "restaurant_name": getattr(settings, "RESTAURANT_NAME", "My Restaurant"),
-        "restaurant_address": getar(setting, "RESTAURANT_ADDRESS","123 Main street, city, country"),
-        "map_embed_url": "https://www.google.com/maps/embed/v1/places?key=YOUR_API_KEY&q=123+main+street+city+country"
+class contactsubmission(models.model):
+    name = models.charfield(max_length=100)
+    email = models.Emailfield()
+    submitted_at = models.dateTimeField(auto_now_add=True)
 
-    }
-    return render(request,"home.html", context)
+    def __str__(self):
+        return f"{self.name} ({ self.email})"
 
 
+#form.py
+from django import forms
+froms .models import contactsubmission
+class contactfrom(forms.modelform):
+    class meta:
+        model = contactsubmission
+        fields = ['name','email']
 
-#home.html
-<!DOCTYPE html> 
-<html>
-<head>
-   <title> {{ restaurant_name }}</title>
-</head>
-<body>
-  <h1> welcome to {{restaurant_name }}</h1>
 
-<h2>find us</h2>
-<p>{{ restaurant_address }}</p>
 
-<iframe
-width="600"
-height="450"
-style="border:0"
-loading="lazy"
-allowfullscreen
-referrerpolicy="no-referrer-when-downgrade"
-src="{{map_embed_url }}">
-</iframe>
-</body>
-</html>
+
+
+
+
+
+
+
+
