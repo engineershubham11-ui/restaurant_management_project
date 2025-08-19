a2d551b349ee1447baede6ba2a50e6106a0d96cd
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-19 21:05:40 +0530

    35448

--- account/views.py
+++ account/views.py
@@ -1,26 +1,43 @@
-from rest_Framework.decorators import api_view
-from rest_Framework.response import Response
+#models.py
+from django.db import models 
 
-@api_view(['GET'])
-def get_menu(request):
-    menu = [
-        {
-            "name": "paneer butter masala",
-            "description": "delicious cottage cheese in a creamy tomato gravy"
-            "price":250
-        },
-        {
-            "name": "chicken biryanni",
-            "description": "fragrant bsamati rice coocked with spiced chicken",
-            "price":300
-        },
-        {
-            "name": "veg fried rice",
-            "description": "stir-fried rice with mixed vegetables and soy sauce",
-            "price": 180
-        }
-    ]
-return Response(menu)
+class menu(models.model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digit=6, decimal_places=2)
+
+
+    def __str__(self):
+        return self.name
+
+
+class order(models.model):
+    customer_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=true)
+    items = models.ManyToManyField(menu)
+
+
+    def __str__(self):
+        return f"order #{self.id} by {self.customer_name}"
+
+
+#admin.py
+
+from django.contrib import admin 
+from .models import menu, order
+
+@admin.register(menu)
+class MenuAdmin(admin.ModeIAdmin):
+    list_display = ("name" , "price")
+    search_fields = ("name",)
+
+@admin.register(order)
+class orderAdmin(admin.modelAdmin):
+    list_display = ("id", "customer_name","created_at")
+    search_fields = ("customer_name",)
+    filter_horizontal = ("items",)
+
+
 
 
 
