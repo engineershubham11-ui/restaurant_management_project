17bbb886e3758dcb2bc71342404baaba9c2009d3
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-20 23:30:18 +0530

    35446

--- account/views.py
+++ account/views.py
@@ -1,52 +1,12 @@
-#models.py
-from django.db import models 
-from django.contrib.auth.models import user
+import request 
+from django.shortcuts import render 
 
-class menu(models.model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digit=6, decimal_places=2)
+def homepage (request):
 
+    response = request.get("http://127.0.0.1:8000/api/menu/")
+    menu_items = response.json() if response.status_code == 200 else []
 
-    def __str__(self):
-        return self.name
-
-
-class order(models.model):
-    STATUS_CHOICES =[
-        ('PENDING', 'Pending'),
-        ('PROCESSING', 'Processing'),
-        ('COMPLETED', 'Completed')
-        ('CANCELLED', 'cancelled')
-    ]
-
-    customer = Models.ForeignKey(user, on_delete=models.CASCADE, RELATED_name="orders")
-    item = models.ManyToManyField(menu, related_name="orders")
-    total_amount = model.DecimalField(max_length=8, decimal_places=2)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES,default='PENDING')
-    created_at = models.DateTimeField(auto_now_add=True)
-
-
-    def __str__(self):
-        return f"order #{self.id} by {self.customer.username} -{self.status}"
-
-
-#admin.py
-
-from django.contrib import admin 
-from .models import menu, order
-
-@admin.register(menu)
-class MenuAdmin(admin.ModeIAdmin):
-    list_display = ("name" , "price")
-    search_fields = ("name",)
-
-@admin.register(order)
-class orderAdmin(admin.modelAdmin):
-    list_display = ("id", "customer","total_amount", "status", "created_at")
-    search_fields = ("customer_name",)
-    filter_horizontal = ("items",)
-
+    return render(request, "homepage.html", {"menu_items": menu_items})
 
 
 
