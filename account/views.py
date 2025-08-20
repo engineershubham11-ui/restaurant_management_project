211afd0b9323e5e5b7e01c19ea2abee4848a4e15
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-20 23:45:35 +0530

    36729

--- account/views.py
+++ account/views.py
@@ -1,30 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import user
 
-class userprofile(models.model):
-    user = models.oneToOneField(user, on_delete=model.CASCADE)
+class Restaurantlocation(models.model):
+    address = models.charfield(max_length=255)
+    city = models.charfield(max_length=100)
+    state = models.charfield(max_length=100)
+    zip_code = models.charfield(max_length=10)
 
-    name = models.charfield(max_length=100)
-    phone_number = models.charfield(max_length=15)
 
     def __str__(self):
-        return self.user.username
-
-
-
-#model.py 
-
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-@receiver(post_save, sender=user)
-def created_or_update_user_profile(sender,instance, created,**kwargs):
-    if created:
-        userprofile.object.create(user=instance, name=instance.get_full_name())
-    else:
-        instance.userprofile.save()
-
-
+        return f"{self.address}, {self.city}, {self.state} -{self.zip_code}"
 
 
 
