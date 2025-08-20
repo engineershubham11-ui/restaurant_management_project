82b048d6e89852a8e14169792dd76297591333e6
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-20 23:39:13 +0530

    35445

--- account/views.py
+++ account/views.py
@@ -1,13 +1,28 @@
-import request 
-from django.shortcuts import render 
+from django.db import models
+from django.contrib.auth.models import user
 
-def homepage (request):
+class userprofile(models.model):
+    user = models.oneToOneField(user, on_delete=model.CASCADE)
 
-    response = request.get("http://127.0.0.1:8000/api/menu/")
-    menu_items = response.json() if response.status_code == 200 else []
+    name = models.charfield(max_length=100)
+    phone_number = models.charfield(max_length=15)
 
-    return render(request, "homepage.html", {"menu_items": menu_items})
+    def __str__(self):
+        return self.user.username
 
+
+
+#model.py 
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=user)
+def created_or_update_user_profile(sender,instance, created,**kwargs):
+    if created:
+        userprofile.object.create(user=instance, name=instance.get_full_name())
+    else:
+        instance.userprofile.save()
 
 
 
