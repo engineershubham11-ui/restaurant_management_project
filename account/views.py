26bac033676c989a8f082f54d44f75cc9ff2afd5
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-21 12:31:09 +0530

    36726

--- account/views.py
+++ account/views.py
@@ -4,27 +4,13 @@ class contact(models.model):
     name = models.charfield(max_length=100)
     description = models.Textfield()
     price = models.DecimalField(max_digit=6, decimal_places=2)
-    image = models.ImageField(upload_to='menu_image/', blank=true, null=True)
+    
+    image = models.ImageField(upload_to='menu_image/', blank=True, null=True)
 
 
     
     def __str__(self):
         return self.name
-
-
-# views.py
-
-from django.shortcuts import render
-from .models import MenuItem
-
-def menu_view(request):
-    menu_items = menuItems.object.all()
-    return render(request, 'menu.html', {'menu_items': menu_items})
-
-
-
-
-
 
 
 
