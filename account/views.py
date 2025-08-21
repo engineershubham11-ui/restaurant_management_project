d48c45b37b050f51b1bb6f5def45dcb0dfc9a8a0
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-21 12:25:01 +0530

    36727

--- account/views.py
+++ account/views.py
@@ -2,12 +2,24 @@ from django.db import models
 
 class contact(models.model):
     name = models.charfield(max_length=100)
-    email = models.emailfield()
+    description = models.Textfield()
+    price = models.DecimalField(max_digit=6, decimal_places=2)
+    image = models.ImageField(upload_to='menu_image/', blank=true, null=True)
 
-    submittted_at = Models.dateTimeField(auto_now_add=True)
 
+    
     def __str__(self):
-        return f"{self.name}, ({self.email})"
+        return self.name
+
+
+# views.py
+
+from django.shortcuts import render
+from .models import MenuItem
+
+def menu_view(request):
+    menu_items = menuItems.object.all()
+    return render(request, 'menu.html', {'menu_items': menu_items})
 
 
 
