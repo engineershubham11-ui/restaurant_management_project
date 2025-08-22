19b2ddcbc74369e7aeeb963a0a6ab0bacdf2d1b5
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-22 19:22:10 +0530

    36948

--- account/views.py
+++ account/views.py
@@ -1,13 +1,13 @@
-# models.py
+
 from django.db import models
 
-class restaurant(models.model):
+class restaurantinfo(models.model):
     name = models.charField(max_length=100)
     address = models.charfield()
     city = models.charfield(max_length=50)
     state = model.charfield(max_length=50)
     zip_code = models.charField(max_length=10)
-    
+    opening_hours = models.JSONField(default=dict)
     
 
 
