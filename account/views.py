525a3e9f142e2b14216d1df9d31f06f0dda68d63
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-22 19:43:23 +0530

    36946

--- account/views.py
+++ account/views.py
@@ -1,21 +1,20 @@
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.emailbackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS =True
+EMAIL_USE_TLS = 'YOUR_EMAIL@GAMIL.COM'
+EMAIL_HOST_PASSWORD = 'your_email_password'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-from django.db import models
 
-class menuinfo(models.model):
-    name = models.charField(max_length=100)
-    description = models.textfield()
-    prices = models.decimalfield(max_length=6, decimal_places=2)
-    image  = model.imagefield(upload_to='menu_images/', blank=true, null=true)
 
-   
+
+
+
+ django.db import models
+
+class contactform(forms.form):
+    name = forms.charField(max_length=100)
+    email  = forms.emailfield()
+    message = forms.charfield(width=forms.Textarea)    
     
-    def __str__(self):
-        return self.name
-
-
-
-setting.py
-
-import os
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
