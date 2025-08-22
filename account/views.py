603fe602daff7d2992687e06fe15b5a84e0dd838
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-22 19:51:46 +0530

    36945

--- account/views.py
+++ account/views.py
@@ -1,20 +1,12 @@
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.emailbackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS =True
-EMAIL_USE_TLS = 'YOUR_EMAIL@GAMIL.COM'
-EMAIL_HOST_PASSWORD = 'your_email_password'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+from django.shortcuts import render
+from .models import menuitem
 
+def homepage(request):
+    query = request.GET.get('q')
+    if query:
+        items = menuitem.object.filter(name__icontains=query)
+        else:
+            items = menuitem.object.all()
 
-
-
-
-
- django.db import models
-
-class contactform(forms.form):
-    name = forms.charField(max_length=100)
-    email  = forms.emailfield()
-    message = forms.charfield(width=forms.Textarea)    
-    
+        return render(request, 'homepage.html',{'items': items, 'query': query})
+        
