9c603f989be4025f0cf5800e085ceb4ad00f1d77
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-23 20:00:56 +0530

    37852

--- account/views.py
+++ account/views.py
@@ -1,12 +1,40 @@
-from django.shortcuts import render
-from .models import menuitem
+from django.db import models 
 
-def homepage(request):
-    query = request.GET.get('q')
-    if query:
-        items = menuitem.object.filter(name__icontains=query)
-        else:
-            items = menuitem.object.all()
+class Restaurant(models.model):
+    name = models.CharField(max_length=100)
+    address = models.TextFeild()
+    phone_number = models.CharField(max_length=15)
 
-        return render(request, 'homepage.html',{'items': items, 'query': query})
-        
+    def __str__(self):
+        return self.name 
+
+
+
+
+#bash
+python manage.py makemigrations
+python manage.py migrate
+
+
+
+#views.py
+
+from django.shortcuts import render 
+from .models import Restaurant 
+
+ def home(request):
+    restaurant = Restaurant.object.first()
+    return render(request, 'home.html', {'restaurant': restaurant})
+
+
+<!DOCTYPE html>
+<html>
+<head>
+   <title>{{ restaurant.name }}</title>
+</head>
+<body>
+    <h1> welcome to {{ restaurant.name }}</h1>
+    <p>Address: {{ restaurant.address }}</p>
+    <p>Phone: <a href="tel:{{ restaurant.phone_number }}">{{ restaurant.phone_number}}</a></p>
+</body>
+</html>
