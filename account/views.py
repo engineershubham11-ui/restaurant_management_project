148d31d5f5f2d832a00d616fa5aed60ced99ab00
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-23 20:19:42 +0530

    37851

--- account/views.py
+++ account/views.py
@@ -4,6 +4,8 @@ class Restaurant(models.model):
     name = models.CharField(max_length=100)
     address = models.TextFeild()
     phone_number = models.CharField(max_length=15)
+    logo = models.ImageField(upload_to='restaurant_logos/', blank=True, null=True)
+
 
     def __str__(self):
         return self.name 
@@ -16,16 +18,37 @@ python manage.py makemigrations
 python manage.py migrate
 
 
+#setting.pr
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 
 #views.py
 
-from django.shortcuts import render 
+from django.conf import setting
+from django.conf.url.static import static 
+
+ urlpattern = [
+    #...
+ ]
+
+if setting.DEBUG:
+    urlpattern += static(setting.MEDIA_URL, document_root=setting.MEDIA_ROOT)
+
+
+
+
+#updateview
+from django.shotcuts import render
 from .models import Restaurant 
 
  def home(request):
     restaurant = Restaurant.object.first()
-    return render(request, 'home.html', {'restaurant': restaurant})
+    return render(request, 'home.html', {'restaurant' : restaurant})
 
+
+
+#home.html
 
 <!DOCTYPE html>
 <html>
@@ -33,8 +56,17 @@ from .models import Restaurant
    <title>{{ restaurant.name }}</title>
 </head>
 <body>
+   <header>
+      {% if restaurant.logo %}
+        <img src ="{{ restaurant.logo.url }}" alt="{{ restaurant.name }} logo" style="max-width:150px;">
+        {% else %}
+          <p> no logo available</p>
+        {% endif %}
+    </header>
+
     <h1> welcome to {{ restaurant.name }}</h1>
     <p>Address: {{ restaurant.address }}</p>
-    <p>Phone: <a href="tel:{{ restaurant.phone_number }}">{{ restaurant.phone_number}}</a></p>
+    <p> Phone: <a href="tel: {{restaurant.phone_number }}">{{ restaurant.phone_number }}</a></p>
+    
 </body>
 </html>
