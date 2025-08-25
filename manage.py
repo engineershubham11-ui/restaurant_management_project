d965d732837be1be89592c22bd0b26c9c15eeb76
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-25 18:27:16 +0530

    37848

--- manage.py
+++ manage.py
@@ -1,22 +1,35 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
+from django import forms
+
+class ContactForm(forms.form):
+    name = forms.charfield(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
 
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_management.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+    def clean_message(Self):
+        message = self.cleaned_data.get('message')
+        if len(message.strip()) < 10:
+            raise forms.validationError("Message should be at least 10 character long.")
+    return message
 
 
-if __name__ == '__main__':
-    main()
+#view.py
+
+from django.shortcuts import render
+from .froms import ContactForm
+
+def contact_view(request):
+    if request.method == "post":
+        form = contactForm(request.POST)
+    if form.is_valid():
+        name = form.cleaned_data['name']
+        email = form.cleaned_data['email']
+        message = form.cleaned_data['message']
+
+        print(f"new message from {name} ({email}): {message}")
+        return render(request, "contact_success.html")
+
+else:
+    form = contactForm()
+
+    return render(request, "contact.html", {"form": form})
