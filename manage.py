8d850f19beb22c7fd0cc53044c01788f72665364
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-25 18:47:05 +0530

    39101

--- manage.py
+++ manage.py
@@ -1,35 +1,39 @@
-from django import forms
+from django.db import models
 
-class ContactForm(forms.form):
-    name = forms.charfield(max_length=100, required=True)
-    email = forms.EmailField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+class RestaurantInfo(models.model):
+    name = models.charfield(max_length=100)
+    email = models.CharField(max_length=15)
+    message = models.TextField(blank=True, null=True)
 
 
-    def clean_message(Self):
-        message = self.cleaned_data.get('message')
-        if len(message.strip()) < 10:
-            raise forms.validationError("Message should be at least 10 character long.")
-    return message
+
+    def __str__(Self):
+        return self.name
+
+
+
+from django.contrib import admin
+from .models import RestaurantInfo
+
+
+admin.site.register(RestaurantInfo)
+
+
 
 
 #view.py
 
 from django.shortcuts import render
-from .froms import ContactForm
+from .models import RestaurantInfo
 
-def contact_view(request):
-    if request.method == "post":
-        form = contactForm(request.POST)
-    if form.is_valid():
-        name = form.cleaned_data['name']
-        email = form.cleaned_data['email']
-        message = form.cleaned_data['message']
+def homepage(request):
+   restaurant = RestaurantInfo.object.first()
+   return render(request, 'homepage.html',{'restaurant': restaurant})
 
-        print(f"new message from {name} ({email}): {message}")
-        return render(request, "contact_success.html")
 
-else:
-    form = contactForm()
 
-    return render(request, "contact.html", {"form": form})
+<div style="text-align: center; margin-top: 10px">
+  <p style="font-size: 1.2rem; font-weight: bold; colour: #333;">
+  call us:<a href = "tel:{{ restaurant.phone }}" style="colour: #007BFF;">{{ restaurant.phone }}</a>
+  </p>
+</div>
