b1496d4286cb82f1904b9b32eafb9c8bede83c33
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-25 20:38:56 +0530

    39100

--- manage.py
+++ manage.py
@@ -1,39 +1,15 @@
-from django.db import models
-
-class RestaurantInfo(models.model):
-    name = models.charfield(max_length=100)
-    email = models.CharField(max_length=15)
-    message = models.TextField(blank=True, null=True)
-
-
-
-    def __str__(Self):
-        return self.name
-
-
-
-from django.contrib import admin
-from .models import RestaurantInfo
-
-
-admin.site.register(RestaurantInfo)
-
-
-
-
-#view.py
-
-from django.shortcuts import render
-from .models import RestaurantInfo
-
-def homepage(request):
-   restaurant = RestaurantInfo.object.first()
-   return render(request, 'homepage.html',{'restaurant': restaurant})
-
-
-
-<div style="text-align: center; margin-top: 10px">
-  <p style="font-size: 1.2rem; font-weight: bold; colour: #333;">
-  call us:<a href = "tel:{{ restaurant.phone }}" style="colour: #007BFF;">{{ restaurant.phone }}</a>
-  </p>
+#homepage.html
+<div style="text-align: center; padding: 40px 20px; background: #f8f9fa; border-radius: 8px; margin-botton: 20px;">
+<h1 style ="font-size: 2.5rem; color: #333; margin-bottom: 10px;">
+        Welcome to Gourmet Haven
+</h1>
+<p style ="font-size:1.2rem; colour: #555; max-width: 600px; margin: 0 auto;">
+"delighting your taste buds with authentic flavors and unforfettable dining experiences."
+</p>
 </div>
+
+
+
+#icon&logo
+<h1 style="font-size: 2.5rem; colour: #333; margin-bottom: 10px;">
+<i class="fas fa-utensils" style="colour:#e67e22;"></i> Welcome to Gourmet Haven</h1>
