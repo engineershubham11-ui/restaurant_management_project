d04e2558ef536a5211097e53281aa9e86817ffe8
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-27 23:25:06 +0530

    39098

--- manage.py
+++ manage.py
@@ -1,9 +1,22 @@
-<nav aria-label ="breadcrumb" class="breakcrumb-nav">
-<ul class = "breakcrumb">
-<li><a href="/">Home</a><li>
-<li><a href="/menu">Menu</a></li>
-<li class ="active">pizza</li>
-</ul>
-</nav>
+from django.urls import path
+from . import views
 
+urlpattern = [
+    path('',views.home, name='home'),
+    path('faq/', views.faq, name='faq'),
+]
 
+#views.py
+from django.shortcuts import render
+
+def home(request);
+  return render(request, 'home.html')
+
+  def faq(request):
+    faq = [
+        {"questiion":"what is the purpose of these site?","answer": "this site provide information about our services."},
+        {"questions": "how can i contact support?", "answer":"you can reach us via the contact page or email support@example.com"},
+        {"questions":"do you offer refaunds?", "answer": "yes, refaund are processed within 7 working days"},
+    ]
+
+return render (request, 'faq.html', {'faqs': faqs})
