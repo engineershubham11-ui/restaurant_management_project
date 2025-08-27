139cf3395c8552c82a6c616c3df332e00bf9ca27
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-27 23:31:15 +0530

    39097

--- manage.py
+++ manage.py
@@ -1,22 +1,17 @@
-from django.urls import path
-from . import views
-
-urlpattern = [
-    path('',views.home, name='home'),
-    path('faq/', views.faq, name='faq'),
-]
-
 #views.py
 from django.shortcuts import render
+from datetime import datetime
 
 def home(request);
-  return render(request, 'home.html')
+  current_datetime = datetime.now()
+  return render(request, 'home.html', {'current_datetime': current_datetime})
 
-  def faq(request):
-    faq = [
-        {"questiion":"what is the purpose of these site?","answer": "this site provide information about our services."},
-        {"questions": "how can i contact support?", "answer":"you can reach us via the contact page or email support@example.com"},
-        {"questions":"do you offer refaunds?", "answer": "yes, refaund are processed within 7 working days"},
-    ]
 
-return render (request, 'faq.html', {'faqs': faqs})
+
+from django import template 
+
+register = template.library()
+
+@register.filter
+def formate_datetime(value):
+    return value.strftime("%d-%b-%y | %I:%M 0p%")
