8dc085791173b3a6f663d374a3a50230a3d5410a
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-29 22:12:53 +0530

    39887

--- manage.py
+++ manage.py
@@ -1,35 +1,34 @@
 from django.shortcuts import render
-from django.urls import reverse
-
-def home(request):
-    breadcrumbs = [("home",None)]
-    return render(request,"home.html", {"breadcrumbs": breadcrumbs})
-
-def menu(request):
-    breadcrumbs = [
-        ("Home", reverse("home")),
-        ("Menu", None),
-    ]
-    return render(request, "menu.html", {"breadcrumbs": breadcrumbs})
+import random
 
 def order_confirmation(request):
+    order_number = random.randint(1000, 9999)
     breadcrumbs = [
-        ("Home", reverse("home")),
-        ("Menu", reverse("menu")),
+        ("Home", "/"),
         ("Order configration", None),
     ]
-    return render(request, "order_configration.html",{"breadcrumbs": breadcrumbs})
+    return render(request, "order_configration.html",{
+    "order_number": order_number,
+    "breadcrumbs": breadcrumbs
+    })
 
 #home.html
 
 {% extend "base.html" %}
 
+
 {% block content %}
-{% include "partials/breadcrumbs.html" %}
+<div class="container mt-5 text-center">
+<h2>order configration</h2>
+<p> Thank you! your order has been placed sucessfully.</p>
 
 
-<div class="container mt-3">
-<!--content-->
+{% if order_number %}
+<p><strong>Order Number:</strong> {{ order_number }}</p>
+{% endif %}
+
+<a href="{% url 'home" %}" class="btn btn-primary mt-3"> Return to Home</a>
 
 </div>
 {% endblock %}
+
