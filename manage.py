738cc963076d88439d56cf44284e67669c0c824c
engineershubham11-ui <engineershubham11@gmail.com>
2025-08-29 21:29:37 +0530

    39889

--- manage.py
+++ manage.py
@@ -1,17 +1,14 @@
 #views.py
 from django.shortcuts import render
-from datetime import datetime
 
-def home(request);
-  current_datetime = datetime.now()
-  return render(request, 'home.html', {'current_datetime': current_datetime})
+def privacy_policy(request):
+    return render(request, "privacy_policy.html")
 
+#urls.py
+from django.urls import path
+from . import views
 
-
-from django import template 
-
-register = template.library()
-
-@register.filter
-def formate_datetime(value):
-    return value.strftime("%d-%b-%y | %I:%M 0p%")
+urlpattern = [
+    path("privacy-policy/", views.privacy_policy, name="privacy_policy"),
+    
+]
